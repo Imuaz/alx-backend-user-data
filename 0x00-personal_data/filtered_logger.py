71bfc9bb40494620a8c67a6ea3b56c9cd93a39a9
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-11-02 23:12:36 +0100

    connect to database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -48,7 +48,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector:
     '''connects to the secure database'''
     db_username = os.environ.get('PERSONAL_DATA_DB_USERNAME', 'root')
     db_password = os.environ.get('PERSONAL_DATA_DB_PASSWORD', '')
