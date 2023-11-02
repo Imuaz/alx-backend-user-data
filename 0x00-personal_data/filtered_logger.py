99f5901133b99234d5a5933438af44f27d810582
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-11-02 22:04:05 +0100

    Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,10 +4,9 @@ Personal data Module
 '''
 import os
 import re
-import csv
 from logging import StreamHandler
 from typing import List
-from mysql import connector
+import mysql.connector
 import logging
 
 
@@ -49,7 +48,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> mysql.connector.connection.MySQLConnection:
+def get_db() -> connector.connection.MySQLConnection:
     '''connects to the secure database'''
     user = os.getenv('PERSONAL_DATA_DB_USERNAME')
     password = os.getenv('PERSONAL_DATA_DB_PASSWORD')
