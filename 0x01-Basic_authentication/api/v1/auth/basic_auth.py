45d3bbbe47784d5d87cfd899d46e11b0860b042e
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-11-08 00:38:44 +0100

    Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ Basic auth module
 """
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -52,7 +53,7 @@ class BasicAuth(Auth):
 
     def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):  # nopep8
         """Retrieve the User instance based on email and password."""
-        if not (user_email and isinstance(user_email, str) and user_pwd and isinstance(user_pwd, str)):
+        if not (user_email and isinstance(user_email, str) and user_pwd and isinstance(user_pwd, str)):  # nopep8
             return None
 
         users = User.search({"email": user_email})
