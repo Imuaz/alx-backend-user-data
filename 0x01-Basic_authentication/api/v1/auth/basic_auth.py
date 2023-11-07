81eaea8b2b2e3500f7f55ebd44ebcc4ec148ea8d
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-11-07 23:39:59 +0100

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -30,10 +30,12 @@ def decode_base64_authorization_header(self, base64_authorization_header: str) -
     """
     Decode a Base64 Authorization header and return as a UTF-8 string.
     """
-    if not base64_authorization_header or not isinstance(base64_authorization_header, str):  # nopep8
+    if not base64_authorization_header:
         return None
-
+    if type(base64_authorization_header) is not str:
+        return None
     try:
-        return b64decode(base64_authorization_header).decode('utf-8')
-    except (binascii.Error, UnicodeDecodeError):
+        decoded_string = base64.b64decode(base64_authorization_header)
+        return decoded_string.decode('utf-8')
+    except Exception:
         return None
