8b1683623ff815e574c9ef3c82404f317e06773e
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-11-07 22:25:17 +0100

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,14 +8,30 @@ from typing import List, TypeVar
 
 class Auth:
     """Authentication class"""
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        # This method is a placeholder for future implementation
-        return False
+
+    def __init__(self, excluded_paths: List[str] = []):
+        self.excluded_paths = excluded_paths
+
+    def require_auth(self, path: str) -> bool:
+        """Check if authentication is required for the given path."""
+        if not self.excluded_paths:
+            return True
+
+        if path in self.excluded_paths or path.rstrip('/') in self.excluded_paths:  # nopep8
+            return False
+
+        for excluded_path in self.excluded_paths:
+            if excluded_path.endswith('*') and path.startswith(excluded_path[:-1]):  # nopep8
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
-        # This method is a placeholder for future implementation
+        """Get the Authorization header from the request."""
+        if request and 'Authorization' in request.headers:
+            return request.headers['Authorization']
         return None
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        # This method is a placeholder for future implementation
+    def current_user(self, request=None) -> TypeVar('user'):
+        """Get the current user (placeholder)."""
         return None
