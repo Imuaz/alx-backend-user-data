4e99b85f2704c96bbfff916c462d9763f82304a8
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-11-15 16:45:02 +0100

    User model implemented

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -4,9 +4,9 @@ User Module
 '''
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
-
 Base = declarative_base()
+
+
 class User(Base):
     '''Defines a user schema'''
     __tablename__ = 'users'
