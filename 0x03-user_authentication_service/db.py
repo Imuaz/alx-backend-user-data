3c4d9b8524dc87e9839847e1499ab6f286390b9d
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-11-16 21:11:20 +0100

    Find user implemented

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 
 from user import Base, User
 
@@ -37,3 +38,18 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs):
+        """
+        Finds first row found in the users table filtered by its arguments
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound as e:
+            raise e
+        except InvalidRequestError as e:
+            raise e
+        finally:
+            self._session.close()
+
+        return user
