1c730c2e1d7172c721a7b553f8bcc23c52cd2b70
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-11-16 20:09:39 +0100

    add: create user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -17,7 +17,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -33,7 +33,6 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Create a user and save it to the database"""
-        
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
