8a3b0bf3bfac1dd9b6c030bafecfb77fbbad9171
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-11-19 22:43:45 +0100

    update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -82,6 +82,16 @@ class Auth:
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
 
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Updates a user's password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError()
+
+        hashed = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=hashed, reset_token=None)
+
 
 def _hash_password(password: str) -> bytes:
     """Returns bytes as asalted hash of the input password, hashed"""
