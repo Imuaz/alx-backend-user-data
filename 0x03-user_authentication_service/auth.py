70a66fb07a9d876eafc18ef12ed317fb30cd9628
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-11-19 21:07:33 +0100

    destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -69,7 +69,10 @@ class Auth:
 
     def destroy_session(self, user_id: int) -> None:
         """Updates the corresponding user's session ID to None"""
-        self._db.update_user(user_id, session_id=None)
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except ValueError:
+            return None
 
 
 def _hash_password(password: str) -> bytes:
